046c4ccedd48289c579d81364d92e834365ebe91
sklonely <asd195375@gmail.com>
2018-06-07 18:03:29 +0800

    更新參數

--- 渾沌系統/混沌抑制.py
+++ 渾沌系統/混沌抑制.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
 
 x1 = [-0.72]
 x2 = [-0.64]
@@ -15,26 +17,24 @@ b = -0.6013
 c = 2
 d = 0.5
 
-for i in range(300):
-    
+for i in range(10000):
+
     e1.append(x1[i] - y1[i])
     e2.append(x2[i] - y2[i])
     x1.append((x1[i]**2) - (x2[i]**2) + a * (x1[i]) + b * x2[i])
-    y2.append(2 * x1[i] * x2[i] + c * x1[i] + d * x2[i])
-    x1.append((y1[i]**2) - (y2[i]**2) + a * (y1[i]) + b * y2[i])
+    x2.append(2 * x1[i] * x2[i] + c * x1[i] + d * x2[i])
+    y1.append((y1[i]**2) - (y2[i]**2) + a * (y1[i]) + b * y2[i])
     y2.append(2 * y1[i] * y2[i] + c * y1[i] + d * y2[i])
-"""
-    e1[i] = x1[i] - y1[i]
-    e2[i] = x2[i] - y2[i]
 
-    x1[i + 1] = (x1[i]**2) - (x2[i]**2) + a * (x1[i]) + b * x2[i]
-    x2[i + 1] = 2 * x1[i] * x2[i] + c * x1[i] + d * x2[i]
-
-    y1[i + 1] = (y1[i]**2) - (y2[i]**2) + a * (y1[i]) + b * y2[i]
-    y2[i + 1] = 2 * y1[i] * y2[i] + c * y1[i] + d * y2[i]
-"""
-
-plt.plot(x1, ".", y1, "o")
-plt.xlabel('K')
-plt.ylabel('X1 & Y1')
+plt.figure()
+plt.subplot(111)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.plot(
+    x1,
+    x2,
+    ".",
+)
+plt.xlabel("x1", fontsize=24)
+plt.ylabel("x2", fontsize=24)
 plt.show()
