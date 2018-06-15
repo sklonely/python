afcc21128c368488bdb5cf2295fcc23321b771f5
sklonely <asd195375@gmail.com>
2018-06-16 01:14:41 +0800

    import auto fix重寫

--- 模組化py/import_auto_pip.py
+++ 模組化py/import_auto_pip.py
@@ -1,33 +1,21 @@
-def importFix():
-    lestModName = ""
-    while 1:
-        try:
-            import sys
-            import os
-            # 要import的東西放這裡
-            import aaa
-        except ModuleNotFoundError:  # 如果找不到模組就進行pip
-            err = str(sys.exc_info()[1])
-            if (lestModName != err[17:-1]):
-                print("缺少mod: " + err[17:-1] + " 正在進行安裝")
-                os.system('pip install ' + err[17:-1])
-                lestModName = err[17:-1]
-            else:
-                print("無法修復import問題 請人工檢查")
-                print("Error mod name: " + err[17:-1])
-                sys.exit()
-        except ImportError:  # 如果找不到模組就進行pip
-            err = str(sys.exc_info()[1])
-            if (lestModName != err[17:-1]):
-                print("缺少mod: " + err[17:-1] + " 正在進行安裝")
-                os.system('pip install ' + err[17:-1])
-                lestModName = err[17:-1]
-            else:
-                print("無法修復import問題 請人工檢查")
-                print("Error mod name: " + err[17:-1])
-                sys.exit()
+# import自動修復 程式碼片段Stste
+lestModName = ""
+while 1:
+    try:
+        import sys
+        import os
+        # 要import的東西放這下面
+        import aaa
+    except ModuleNotFoundError:
+        err = str(sys.exc_info()[1])[17:-1]
+        if (lestModName != err):
+            print("缺少mod: " + err + " 正在嘗試進行安裝")
+            os.system("pip install " + err)
+            lestModName = err
         else:
-            break
-
-
-importFix()
+            print("無法修復import問題 請人工檢查", "mod name: " + err)
+            sys.exit()
+    else:
+        del lestModName, err
+        break
+# import自動修復 程式碼片段
