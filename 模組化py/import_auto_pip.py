1fd7021edc5c337b92ddcf1fb086f7ba5c139b36
sklonely <asd195375@gmail.com>
2018-06-22 02:26:09 +0800

    import auto pip 新寫法

--- 模組化py/import_auto_pip.py
+++ 模組化py/import_auto_pip.py
@@ -5,17 +5,8 @@ while 1:
         import sys
         import os
         # 要import的東西放這下面
-        import aaa
-    except ModuleNotFoundError:  # python 3.5以上適用
-        err = str(sys.exc_info()[1])[17:-1]
-        if (lestModName != err):
-            print("缺少mod: " + err + " 正在嘗試進行安裝")
-            os.system("pip install " + err)
-            lestModName = err
-        else:
-            print("無法修復import問題 請人工檢查", "mod name: " + err)
-            sys.exit()
-    except ImportError:  # python 3.5以下適用
+
+    except (ModuleNotFoundError, ImportError):  # python import error
         err = str(sys.exc_info()[1])[17:-1]
         if (lestModName != err):
             print("缺少mod: " + err + " 正在嘗試進行安裝")
@@ -25,6 +16,6 @@ while 1:
             print("無法修復import問題 請人工檢查", "mod name: " + err)
             sys.exit()
     else:
-        del lestModName, err
+        del lestModName
         break
 # import自動修復 程式碼片段
