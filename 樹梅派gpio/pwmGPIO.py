50e2542e31006b7c17b753c4d81bbbe0aee5059d
sklonely <asd195375@gmail.com>
2018-07-06 18:23:10 +0800

    更新

---
+++ 樹梅派gpio/pwmGPIO.py
@@ -0,0 +1,40 @@
+# import自動修復 程式碼片段Stste
+lestModName = ""
+while 1:
+    try:
+        import sys
+        import os
+        sys.path.append(sys.path[0] + '/mods/')  # 將自己mods的路徑加入倒python lib裡面
+        # 要import的東西放這下面
+        import RPi.GPIO as GPIO
+        import time
+    except (ModuleNotFoundError, ImportError):  # python import error
+        err = str(sys.exc_info()[1])[17:-1]
+        if (lestModName != err):
+            print("缺少mod: " + err + " 正在嘗試進行安裝")
+            os.system("pip install " + err)
+            lestModName = err
+        else:
+            print("無法修復import問題 請人工檢查", "mod name: " + err)
+            sys.exit()
+    else:
+        del lestModName
+        break
+# import自動修復 程式碼片段
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+
+pwm = GPIO.PWM(2, 80)
+
+pwm.start(0)
+
+while 1:
+    for i in range(0, 101, 1):
+        pwm.ChangeDutyCycle(i)
+        time.sleep(.02)
+
+    for i in range(100, -1, -1):
+        pwm.ChangeDutyCycle(i)
+        time.sleep(.02)
--- 測試用/實驗用檔案.py
+++ 測試用/實驗用檔案.py
@@ -1,6 +1,6 @@
 a = 'thread-11728982-1-BX4TQDHP.html'
 a = a.split("-1-")
 
-for i in range(10):
-    b = a[0] + "-" + str(i + 1) + "-" + a[1]
-    print(b)
+i = 0
+i = 2
+i = 3
