bed2ef34062e578707d97fc82aeadb14594c61d4
sklonely <asd195375@gmail.com>
2018-07-14 00:49:21 +0800

    SEVER問題OK 剩下速度

--- 渾沌系統/sever.py
+++ 渾沌系統/sever.py
@@ -5,6 +5,7 @@ import random
 import json
 import time
 import threading
+import copy
 import hashlib
 app = Flask(__name__)
 # 變數
@@ -26,8 +27,11 @@ def encrypt():
 @app.route("/AES_encrypt")
 def AES_encrypt(data="你好世界"):
     # 加密資料
-    temp_Um = Um
-    key = X[0]
+    global ss, sy
+    temp_Um = copy.deepcopy(Um)
+    sy = copy.deepcopy(X)
+    ss = temp_Um
+    key = sy[0]
     aes = AEScharp()
     sendData = aes.encrypt(data, key)
     global testEN
@@ -40,18 +44,19 @@ def AES_encrypt(data="你好世界"):
 @app.route("/decrypt")
 def decrypt():
     # 同步key值
-    temp_X = X
-    temp_Um = Um
+    temp_X = sy
+    temp_Um = ss
     Y = [random.random(), random.random(), random.random()]
     client = Chaos()
-    for i in range(19, -1, -1):
+    time.sleep(2)
+    for i in range(9, -1, -1):
         Y = client.runSlave(2, Y, temp_Um[i])
         UK = client.createUk(temp_X, Y)
+
     print(temp_X[0], Y[0], UK)
     # 解密
     aes = AEScharp()
     getData = aes.decrypt(testEN, Y[0])
-    # print(temp_Um)
     return str(getData)
 
 
@@ -61,16 +66,17 @@ def chaos():
     global X, Um
     X = [-1.3156345, -1.84, 0.5624]
     Um = []
-    for i in range(20):
+    for i in range(10):
         Um.append(0)
     Um[0] = sys_chaos.createUm(X)
     X = sys_chaos.runMaster(0, X)
     # 進入迴圈開始跑渾沌
     while 1:
-        for i in range(19, 0, -1):
+        for i in range(9, 0, -1):
             Um[i] = Um[i - 1]
         Um[0] = sys_chaos.createUm(X)
         X = sys_chaos.runMaster(1, X)
+        # print(X[0], Um[0])
         time.sleep(0.1)
 
 
@@ -79,11 +85,13 @@ if __name__ == "__main__":
         sys_chaos = threading.Thread(target=chaos)
         sys_chaos.setDaemon(True)
         sys_chaos.start()
+        # 測試寒士
         time.sleep(2)
-        AES_encrypt()
         for i in range(10):
+            AES_encrypt()
             time.sleep(1)
             decrypt()
+
         # app.run()
     except 1:
         print("退出渾沌加密系統")
